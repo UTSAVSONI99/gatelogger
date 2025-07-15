98b2ae914a21e70d21df9e14431edf884101d554
Utsav Soni <utsavsoni619@gmail.com>
2025-07-15 11:18:04 +0530

    setup prisma

---
+++ prisma/seed.ts
@@ -0,0 +1,12 @@
+import { PrismaClient, Prisma } from "@/generated/prisma";
+
+const prisma = new PrismaClient();
+
+const users :Prisma.userCreateInput[] =[
+    
+]
+
+async function main(){
+
+}
+main()
